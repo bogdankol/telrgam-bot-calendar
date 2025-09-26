f09807239d90e334603c4afa4845cd7ff5ef7d01
bogdankol <68349689+bogdankol@users.noreply.github.com>
2025-09-26 16:34:08 +0300

    edited route.ts

--- src/app/api/telegram-bot/route.ts
+++ src/app/api/telegram-bot/route.ts
@@ -56,7 +56,7 @@ bot.on("text", async (ctx) => {
     }
 
     const response = await calendar.events.insert({
-      calendarId: GOOGLE_CALENDAR_ID!,
+      calendarId: CALENDAR_ID!,
       requestBody: event,
       conferenceDataVersion: 1,
     })
