2ad77fc213b34a84f3823d4babe1319e9023bded
bogdankol <68349689+bogdankol@users.noreply.github.com>
2025-10-11 16:07:09 +0300

    reseted last commit and removed some code

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -103,14 +103,3 @@ export function isValidPhone(phone: string) {
   if (!/^\+?\d{9,15}$/.test(cleaned)) return false; // длина номера
   return true;
 }
-
-// export function transformIntoArrayOfCortages<T extends Record<string, any>>(obj: T) {
-// 		const arr = Object.entries(obj)
-
-// 		const arrWithInnerCortages = arr.map(cortage => {
-// 			if (typeof cortage[1] !== 'object') return cortage
-// 			return [cortage[0], Object.entries(cortage[1])]
-// 		})
-
-// 		return arrWithInnerCortages
-// 	}
