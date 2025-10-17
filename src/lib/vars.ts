f120f537d330b739b6562ed9590b13be0940d173
bogdankol <68349689+bogdankol@users.noreply.github.com>
2025-10-17 15:46:32 +0300

    changed meetings message 2

--- src/lib/vars.ts
+++ src/lib/vars.ts
@@ -1,3 +1,6 @@
 export const SCOPES = ['https://www.googleapis.com/auth/calendar']
 export const TIMEZONE = 'Europe/Kiev'
 // export const invoiceCheckUrl = `/invoice-check`
+export const OFFICE_ADDRESS = `вулиця із дуже довгою назвою, місто із довгою назвою, підєїзд із номером 1233213, номер офису 1231233`
+export const OFFLINE_MEETING_MESSAGE = `Зустріч в офісі, за адресою: ${OFFICE_ADDRESS}`
+export const ONLINE_MEETING_MESSAGE = `Онлайн зустріч. Посилання буде надіслано пізніше на вказаний вами email`
